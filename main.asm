cd28d8886ec93a659aad278177db8a0ce20113e2
HyperX98 <raulxiloj98@gmail.com>
2020-03-19 17:39:50 -0600

    Nuevas variables globales

--- main.asm
+++ main.asm
@@ -5,17 +5,25 @@ include macros.asm
 .stack 100h
 ;-----Data segment-----
 .data
-header db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,13,"NOMBRE: RAUL XILOJ",10,13,"CARNET: 201612113",10,13,"SECCION: A",10,10,13,36
-options db "1) Iniciar juego",10,13,"2) Cargar juego",10,13,"3) Salir",10,13,36
-start db "starting game",36
-load db "loading game",36
+header   db "UNIVERSIDAD DE SAN CARLOS DE GUATEMALA",10,13,"FACULTAD DE INGENIERIA",10,13,"CIENCIAS Y SISTEMAS",10,13,"ARQUITECTURA DE COMPUTADORAS Y ENSAMBLADORES 1",10,13,"NOMBRE: RAUL XILOJ",10,13,"CARNET: 201612113",10,13,"SECCION: A",10,10,13,'$'
+options  db "1) Iniciar juego",10,13,"2) Cargar juego",10,13,"3) Salir",10,13,"Ingrese una opcion: ",'$'
+table    db 64 dup('$'),'$'
+filas    db 1h,2h,3h,4h,5h,6h,7h,8h
+columnas db 'A','B','C','D','E','F','G','H'
+newLine  db 10,'$'
+dash     db "---",'$' 
+wall     db "|   |   |   |   |   |   |   |",10,13,'$'
+white    db "W",'$'
+black    db "B",'$'
+space    db " ",'$'
+load     db "loading game",'$'
 ;-----Code segment-----
 .code
 main proc
 
         print header
-        print options
         menuPrincipal:
+                print options
                 getChar
                 cmp al, 31h
                 je startGame
@@ -26,7 +34,9 @@ main proc
                 jmp menuPrincipal
 
         startGame:
-                print start
+                print newLine
+                showTable table,dash,wall,black,white,space,newLine
+                jmp menuPrincipal
         loadGame:
                 print load
         finishGame:
@@ -34,4 +44,5 @@ main proc
                 int 21h
 
 main endp
+
 end main
