aa21885f357a92626931aec6f9b1764e53a1dac3
HyperX98 <raulxiloj98@gmail.com>
2020-03-26 20:47:43 -0600

    Se creo el reporte final

--- reports.asm
+++ reports.asm
@@ -21,62 +21,8 @@ createActualReport macro
     ;Tablero (divs)
     writeFile handler, divTable, SIZEOF divTable
     seekEnd handler
-    ;row1
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row1
-    printRow1
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row2
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row2
-    printRow2
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row3
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row3
-    printRow3
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row4
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row4
-    printRow4
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row5
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row5
-    printRow5
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row6
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row6
-    printRow6
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row7
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row7
-    printRow7
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
-    ;row8
-    writeFile handler, divrow, SIZEOF divrow
-    seekEnd handler
-    ;cols of row8
-    printRow8
-    writeFile handler, divEnd, SIZEOF divEnd
-    seekEnd handler
+    ;PRINT ROWS
+    printRows
     ;div letters
     writeFile handler, divLets, SIZEOF divLets
     seekEnd handler
@@ -98,387 +44,202 @@ createActualReport macro
     closeFile handler 
 endm
 
-printRow1 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],49
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov si,0h
+printRows macro
+LOCAL while, black, white, L2, plus,while2
+    xor cx, cx
+    xor si, si
+    mov cx, 8h
+    
+    mov si, 0
     mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
+    while: 
+        mov bl,48
+        xor dx, dx
+        mov dx, 8h
+        pusha
+        writeFile handler, divrow, SIZEOF divrow
         seekEnd handler
         popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 7h
-        jbe mientras
+        while2:
+            cmp table[si],48
+            je black
+            cmp table[si],49
+            je white
+            jmp L2
+            black:
+                add bl,1
+                mov divX[13],bl
+                mov divX[14],66
+                pusha 
+                writeFile handler,divX, SIZEOF divX
+                seekEnd handler
+                popa
+                jmp plus
+            white:
+                add bl,1
+                mov divX[13],bl
+                mov divX[14],87
+                pusha 
+                writeFile handler,divX, SIZEOF divX
+                seekEnd handler
+                popa
+                jmp plus
+            L2:
+                add bl,1
+                mov divX[13],88
+                mov divX[14],88
+                pusha 
+                writeFile handler,divX, SIZEOF divX
+                seekEnd handler
+                popa
+            plus:
+                inc si
+                dec dx
+        cmp dx,0h
+        jne while2
+    pusha 
+    writeFile handler, divEnd, SIZEOF divEnd
+    seekEnd handler
+    popa
+    dec cx
+    cmp cx, 0h
+    jne while
 endm
 
-printRow2 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],50
-    writeFile handler,divNum, SIZEOF divX
+;Reporte final
+createFinalReport macro 
+    createFile report, handler
+    writeFile handler, doctype, SIZEOF doctype
     seekEnd handler
-    xor cx,cx
-    mov cx,8h
-    mov si,8h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 0Fh
-        jbe mientras
+    writeFile handler, htmlInit, SIZEOF htmlInit
+    seekEnd handler
+    writeFile handler, headInit, SIZEOF headInit
+    seekEnd handler
+    writeFile handler, titleTag, SIZEOF titleTag
+    seekEnd handler
+    ;CSS
+    writeFile handler,  links, SIZEOF links
+    seekEnd handler
+    writeFile handler, headEnd, SIZEOF headEnd
+    seekEnd handler
+    writeFile handler, bodyInit, SIZEOF bodyInit
+    seekEnd handler
+    ;h1 title
+    writeFile handler, h1Title2, SIZEOF h1Title
+    seekEnd handler
+    ;Tablero (divs)
+    writeFile handler, divTable, SIZEOF divTable
+    seekEnd handler
+    ;PRINT ROWS
+    printRows2
+    ;div letters
+    writeFile handler, divLets, SIZEOF divLets
+    seekEnd handler
+    ;--------------
+    writeFile handler, divEnd, SIZEOF divEnd
+    seekEnd handler
+    ;Time 
+    writeFile handler, h1Time, SIZEOF h1Time
+    seekEnd handler
+    writeFile handler, date, SIZEOF date
+    writeFile handler, time, SIZEOF time
+    seekEnd handler
+    writeFile handler, h1Time2, SIZEOF h1Time2
+    seekEnd handler
+    ;End
+    writeFile handler, bodyEnd, SIZEOF bodyEnd
+    seekEnd handler
+    writeFile handler, htmlEnd, SIZEOF htmlEnd
+    closeFile handler 
 endm
 
-printRow3 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],51
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov cx,10h
-    mov si,10h
+printRows2 macro
+LOCAL while, black, white, L2, plus,while2
+    xor cx, cx
+    xor si, si
+    mov cx, 8h
+    
+    mov si, 0
     mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
+    while: 
+        mov bl,48
+        xor dx, dx
+        mov dx, 8h
+        pusha
+        writeFile handler, divrow, SIZEOF divrow
         seekEnd handler
         popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 17h
-        jbe mientras
-endm
-
-printRow4 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],52
-    writeFile handler,divNum, SIZEOF divX
+        while2:
+            cmp table[si],48
+            je black
+            cmp table[si],49
+            je white
+            cmp table[si],50
+            je circle
+            cmp table[si],51
+            je square
+            jmp L2
+            black:
+                add bl,1
+                mov divX[13],bl
+                mov divX[14],66
+                pusha 
+                writeFile handler,divX, SIZEOF divX
+                seekEnd handler
+                popa
+                jmp plus
+            white:
+                add bl,1
+                mov divX[13],bl
+                mov divX[14],87
+                pusha 
+                writeFile handler,divX, SIZEOF divX
+                seekEnd handler
+                popa
+                jmp plus
+            circle:
+                add bl,1
+                mov divSCT[10],99
+                mov divSCT[11],105
+                mov divSCT[12],114
+                mov divSCT[13],bl
+                pusha 
+                writeFile handler,divSCT, SIZEOF divSCT
+                seekEnd handler
+                popa
+                jmp plus
+            square: 
+                add bl,1
+                mov divSCT[10],115
+                mov divSCT[11],113
+                mov divSCT[12],114
+                mov divSCT[13],bl
+                pusha 
+                writeFile handler,divSCT, SIZEOF divSCT
+                seekEnd handler
+                popa
+                jmp plus
+            L2:
+                add bl,1
+                mov divSCT[10],116
+                mov divSCT[11],114
+                mov divSCT[12],105
+                mov divSCT[13],bl
+                pusha 
+                writeFile handler,divSCT, SIZEOF divSCT
+                seekEnd handler
+                popa
+                jmp plus
+            plus:
+                inc si
+                dec dx
+        cmp dx,0h
+        jne while2
+    pusha 
+    writeFile handler, divEnd, SIZEOF divEnd
     seekEnd handler
-    xor cx,cx
-    mov cx,18h
-    mov si,18h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 1Fh
-        jbe mientras
+    popa
+    dec cx
+    cmp cx, 0h
+    jne while
 endm
-
-printRow5 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],53
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov cx,20h
-    mov si,20h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 27h
-        jbe mientras
-endm
-
-printRow6 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],54
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov cx,28h
-    mov si,28h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 2Fh
-        jbe mientras
-endm
-
-printRow7 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],55
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov cx,30h
-    mov si,30h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 37h
-        jbe mientras
-endm
-
-printRow8 macro
-LOCAL mientras,black,white,L2,plus
-    mov divNum[15],56
-    writeFile handler,divNum, SIZEOF divX
-    seekEnd handler
-    xor cx,cx
-    mov cx,38h
-    mov si,38h
-    mov bl,48
-    mientras:
-    cmp table[si],48
-    je black
-    cmp table[si],49
-    je white
-    jmp L2
-    black:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],66
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    white:
-        add bl,1
-        mov divX[13],bl
-        mov divX[14],87
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-        jmp plus
-    L2:
-        add bl,1
-        mov divX[13],88
-        mov divX[14],88
-        pusha 
-        writeFile handler,divX, SIZEOF divX
-        seekEnd handler
-        popa
-    plus:
-        inc cx
-        inc si
-        cmp cx, 3Fh
-        jbe mientras
-endm
-
-
